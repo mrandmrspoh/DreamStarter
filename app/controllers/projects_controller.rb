8dcbb2f2e2356ffe488bccf97ce28f0b2c9696c3
root <root@Sathvik.localdomain>
2019-02-08 12:04:30 +0800

    include user for create project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,31 +3,45 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user!, :except => [ :index ]
 
   def index
+
       @project = Project.all
       @units = Unit.all
       @sectors = Sector.all
       @areas = Area.all
+
   end
 
 
   def new
+
     @units = Unit.all
     @sectors = Sector.all
-    @areas = Area.all
+    @areas = Area.all      
+    
   end
 
 
   def create
+
+    @units = Unit.all
+    @sectors = Sector.all
+    @areas = Area.all
+    
     @project = Project.new(project_params)
+    @project.user_id = current_user.id    
+
     if @project.save
       redirect_to @project
     else
       render 'new'
+      puts @project.errors.messages.inspect
     end
+
   end
 
 
   def show
+
     @project = Project.find(params[:id])
     @units = Unit.all
     @project.user_id = current_user.id
@@ -38,12 +52,14 @@ class ProjectsController < ApplicationController
     @pct_left = 100-@pct_total
     @days_left =  @project.funding_close_date - Date.today
     @target = @project.funding_target.to_i
+
   end
 
 
 private
 
   def project_params
+
     params.require(:project).permit(:name, :objective, :funding_start_date, :funding_close_date, :funding_target, :contact_name, :company_name, :telephone, :email, :image, :video, :website, :facebook, :content_project, :content_company, :content_financials, :content_reports, :faq, :user_id, :sector_id, :area_id, :unit_id, :txn_ids => [])
   end
 
